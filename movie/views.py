3c8c4fc13534eee291fb609a06ac0888a93a5635
d1n2o3b4 <davebostons2017@gmail.com>
2023-11-04 21:51:05 +0000

    modified views

--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from .models import Movie
 # Create your views here.
 
 def home(request):
     searchTerm = request.GET.get('searchMovie')
-    return render(request,'home.html',{'searchTerm':searchTerm})
+    movies = Movie.objects.all()
+    return render(request, 'home.html',{'searchTerm':searchTerm, 'movies':movies})
 
 def about(request):
     return render(request,'about.html',{'name':'David'})
